601b573da4a8344967fe5b6ca444b063de257e1a
Rahulreddy1020 <83365184+Rahulreddy1020@users.noreply.github.com>
2023-08-08 19:12:30 -0700

    Update 0020-valid-parentheses.py

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,3 +1,11 @@
+/// intially we will traverse through the given string, if the string has a character which matches with the key(which is closing brackets) of the hash map 
+/// then we will check if the stack[-1] which is top element, has any open bracket, if so then we will pop the item. the loop will exit when it sees a open bracket,
+/// then we will append that item to stack and again traverse throught the string, at last we will return true if only the stack is empty and matches open and close in order.
+
+
+
+
+
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
