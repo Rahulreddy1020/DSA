630efd837b1cb5ce079e89f14616d45d733b8102
Rahul Reddy <83365184+Rahulreddy1020@users.noreply.github.com>
2023-08-18 07:09:36 -0700

    Time: 72 ms (21.02%), Space: 17.2 MB (61.73%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -18,52 +18,28 @@
 ## will do same for l and r,  and if we found both left.lower and right.lower not equal to each other then we will return false.
 ## if its equal then we increment l and decrement r
 
-# class Solution:
-#     def isPalindrome(self, s: str) -> bool:  
-#         l, r = 0, len(s) - 1
-#         while l < r :
-            
-#             while l < r and not self.alphanum(s[l]): # so when we encounter any non alphanumerical values we move our pointer to next 
-#                 l+= 1
-#             while l < r and not self.alphanum(s[r]):
-#                 r -= 1
-            
-#             if s[l].lower() != s[r].lower() :
-#                 return False
-            
-#             l+=1
-#             r-=1
-            
-#         return True
-    
-#     def alphanum(self, c):  ## creating a function to check if a current character is a alphanumerical using ascii values
-#         return (
-#             ord('A') <= ord(c) <= ord('Z') or 
-#         ord('a') <= ord(c) <= ord('z') or
-#         ord('1') <= ord(c) <= ord('9')
-#                )
-
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
+    def isPalindrome(self, s: str) -> bool:  
         l, r = 0, len(s) - 1
-        while l < r:
-            while l < r and not self.alphanum(s[l]):
-                l += 1
+        while l < r :
+            
+            while l < r and not self.alphanum(s[l]): # so when we encounter any non alphanumerical values we move our pointer to next 
+                l+= 1
             while l < r and not self.alphanum(s[r]):
                 r -= 1
-            if s[l].lower() != s[r].lower():
-                return False
-            l += 1
-            r -= 1
-        return True
-
-    # Could write own alpha-numeric function
-    def alphanum(self, c):
-        return (
-            ord("A") <= ord(c) <= ord("Z")
-            or ord("a") <= ord(c) <= ord("z")
-            or ord("0") <= ord(c) <= ord("9")
-        )
-
-        
             
+            if s[l].lower() != s[r].lower() :
+                return False
+            
+            l+=1
+            r-=1
+            
+        return True
+    
+    def alphanum(self, c):  ## creating a function to check if a current character is a alphanumerical using ascii values
+        return (
+            ord('A') <= ord(c) <= ord('Z') or 
+        ord('a') <= ord(c) <= ord('z') or
+        ord('0') <= ord(c) <= ord('9')
+               )
+
