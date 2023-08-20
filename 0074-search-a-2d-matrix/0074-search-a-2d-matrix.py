5548d7cb45a9d30380fffc1143fff029c96af0fa
Rahul Reddy <83365184+Rahulreddy1020@users.noreply.github.com>
2023-08-19 22:02:01 -0700

    Time: 56 ms (39.51%), Space: 16.8 MB (86.83%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,31 @@
+## intially we will find in which row the target is present by using binary search algo. by finding out if the target is small or larger than the value of the row first and last value, which has been found out by calculating mid.
+
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        ROWS, COLS = len(matrix), len(matrix[0])
+
+        top, bot = 0, ROWS - 1
+        while top <= bot:
+            row = (top + bot) // 2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+
+        if not (top <= bot):
+            return False
+        row = (top + bot) // 2
+        l, r = 0, COLS - 1
+        while l <= r:
+            m = (l + r) // 2
+            if target > matrix[row][m]:
+                l = m + 1
+            elif target < matrix[row][m]:
+                r = m - 1
+            else:
+                return True
+        return False
